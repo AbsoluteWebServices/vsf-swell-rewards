220d6e6258f82aab227c6c0c148983c251131009
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2019-12-23 18:30:32 +0200

    feat(components): Add campaigns component

---
+++ components/Campaigns.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="swell-campaign-list-container">
+    <ul class="swell-campaign-list">
+      <li v-for="campaign in campaigns" :key="campaign.id">
+        <slot :campaign="campaign">
+          <div>
+            <i :class="campaign.icon"/>
+            <h5>{{ campaign.title }}</h5>
+            <p>{{ campaign.reward_text }}</p>
+          </div>
+        </slot>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { KEY } from '../index'
+
+export default {
+  name: 'Campaigns',
+  props: {
+    withStatus: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    ...mapState(KEY, ['campaigns'])
+  },
+  serverPrefetch () {
+    return this.fetchCampaigns()
+  },
+  mounted () {
+    if (!this.item) {
+      this.fetchCampaigns()
+    }
+  },
+  methods: {
+    fetchCampaigns () {
+      return this.$store.dispatch('swell-rewards/getActiveCampaigns')
+    }
+  }
+}
+</script>
