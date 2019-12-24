3e2d47d70bca8586b7cec3d5d41d5d747c03a49a
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2019-12-24 21:00:11 +0200

    feat(compoentns): Clean up campaigns a little

--- components/Campaigns.vue
+++ components/Campaigns.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="swell-campaign-list-container">
-    <slot name="title"></slot>
+    <slot name="title" />
     <ul class="swell-campaign-list">
       <li v-for="campaign in campaigns" :key="campaign.id">
         <slot :campaign="campaign">
           <div>
-            <i :class="campaign.icon"/>
+            <i :class="campaign.icon" />
             <h5>{{ campaign.title }}</h5>
             <p>{{ campaign.reward_text }}</p>
           </div>
@@ -41,7 +41,7 @@ export default {
   },
   methods: {
     fetchCampaigns () {
-      return this.$store.dispatch('swell-rewards/getActiveCampaigns')
+      return this.$store.dispatch(KEY + '/getActiveCampaigns', this.withStatus)
     }
   }
 }
