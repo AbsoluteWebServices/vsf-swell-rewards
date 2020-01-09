b78b665bd9471471238e58ed6a806faa8ce48970
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2020-01-09 14:26:28 +0200

    feat(global): Refresh customer every minute

--- hooks/afterRegistration.ts
+++ hooks/afterRegistration.ts
@@ -1,9 +1,19 @@
 import { KEY } from '../index'
 import * as types from '../store/mutation-types'
+import { setInterval, clearInterval } from 'timers'
 
 export function afterRegistration ({ Vue, config, store, isServer }) {
   if (!isServer && config.swellRewards) {
     Vue.prototype.$bus.$emit('swell:initialized')
+
+    let refreshInterval: NodeJS.Timeout
+    const refresh = () => {
+      try {
+        store.dispatch(KEY + '/refreshCustomer', { with_referral_code: true, with_history: true })
+      } catch (err) {
+        clearInterval(refreshInterval)
+      }
+    }
 
     Vue.prototype.$bus.$on('user-after-loggedin', async receivedData => {
       store.commit(KEY + '/' + types.SET_CUSTOMER_ID, receivedData.id)
@@ -16,11 +26,17 @@ export function afterRegistration ({ Vue, config, store, isServer }) {
         }
       }
       await store.dispatch(KEY + '/getCustomerV1', receivedData.email)
+
+      refreshInterval = setInterval(refresh, 60000)
+
       Vue.prototype.$bus.$emit('swell:setup')
     })
 
     Vue.prototype.$bus.$on('user-before-logout', () => {
       store.commit(KEY + '/' + types.CLEAR)
+
+      clearInterval(refreshInterval)
+
       Vue.prototype.$bus.$emit('swell:setup')
     })
   }
